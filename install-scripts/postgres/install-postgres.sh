af5ab6d4622cf03e927d3c51270442a6bb583cdf
suleonal <79368368+suleonal@users.noreply.github.com>
2024-12-11 18:15:18 +0300

    Add minio install sh

--- install-scripts/postgres/install-postgres.sh
+++ install-scripts/postgres/install-postgres.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 helm upgrade --install postgres bitnami/postgresql -f default-values.yaml
+helm install my-minio minio/minio --set mode=standalone -f minio-default-values.yaml -n databases
